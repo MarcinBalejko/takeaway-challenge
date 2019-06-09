5442748c5ce745bc1c5d418114ec60c1a6ed4bff
MarcinBalejko <mbbm9@o2.pl>
2019-06-10 04:42:44 +0800

    :construction: Passing dish class to list class as an argument

--- lib/list.rb
+++ lib/list.rb
@@ -1,11 +1,14 @@
+require_relative 'dish'
 class List
     attr_reader :menu, :selected_dishes
     def initialize
         @menu = {}
         @selected_dishes = {}
     end
-    def add_dish(dish, price)
-        @menu[dish] = price
+    def add_dish(dish)
+        name = dish.name?
+        price = dish.price?
+        @menu[name] = price
     end
     def select(dish)
         @selected_dishes[dish] = @menu[dish]
