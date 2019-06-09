bf1ed12618d6f83e05499b41da7a687cc584921b
MarcinBalejko <mbbm9@o2.pl>
2019-06-10 02:45:13 +0800

    :white_check_mark: Dish has it's name

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -1,4 +1,11 @@
 require './lib/dish.rb'
 describe Dish do
+
+    let(:subject) { Dish.new("pizza", 10) }
+
+    it 'has a name' do
+        subject
+        expect(subject.name).to eq "pizza"
+    end
     
 end
