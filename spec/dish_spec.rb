07ff807c9cd60562b14229d98689539f93df968f
MarcinBalejko <mbbm9@o2.pl>
2019-06-10 02:47:37 +0800

    :white_check_mark: Has it's price

--- spec/dish_spec.rb
+++ spec/dish_spec.rb
@@ -4,8 +4,10 @@ describe Dish do
     let(:subject) { Dish.new("pizza", 10) }
 
     it 'has a name' do
-        subject
         expect(subject.name).to eq "pizza"
+    end
+    it 'has a price' do
+        expect(subject.price).to eq 10
     end
     
 end
