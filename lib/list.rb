9769d0d0f3408531a0a0d1b0452cbd61ed7cc67f
MarcinBalejko <mbbm9@o2.pl>
2019-06-10 01:41:57 +0800

    :white_check_mark: Select method

--- lib/list.rb
+++ lib/list.rb
@@ -5,7 +5,8 @@ class List
     end
     def add_dish(dish, price)
         @menu[dish] = price
-
+    end
+    def select(dish)
     end
 
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -3,11 +3,18 @@ describe List do
     it 'has menu' do
         expect(subject).to respond_to(:menu)
     end
-    it 'can add dish to the menu' do
-        dish = "pizza"
-        price = 10
-     
-        subject.add_dish(dish, 10)
-        expect(subject.menu.flatten[-2]).to eq dish
+    describe '#add_dish' do
+        it 'can add dish to the menu' do
+            dish = "pizza"
+            price = 10
+            subject.add_dish(dish, 10)
+            expect(subject.menu.flatten[-2]).to eq dish
+        end
     end
+    describe '#select' do
+        it 'responds to select' do
+            expect(subject).to respond_to(:select)
+        end
+    end
+
 end
