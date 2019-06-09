b0fb016a9833e0f64c7ff0f8951407b707fa2e03
MarcinBalejko <mbbm9@o2.pl>
2019-06-10 04:28:40 +0800

    :recycle: Refactored

--- lib/dish.rb
+++ lib/dish.rb
@@ -1,7 +1,14 @@
+require_relative 'list'
 class Dish
     attr_reader :name, :price
     def initialize(name, price)
         @name = name
         @price = price
+    end
+    def name?
+        @name
+    end
+    def price?
+        @price
     end
 end
