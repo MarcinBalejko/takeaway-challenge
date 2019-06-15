be47835d4d5a8e8a2868436a7776092fe3186017
MarcinBalejko <mbbm9@o2.pl>
2019-06-15 20:26:07 +0800

    :white_check_mark: Takeaway class instantiates new menu class

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,14 +1,16 @@
 require_relative 'dish'
 require_relative 'order'
+require_relative 'menu'
 class Takeaway
-    attr_reader :menu, :selected_dishes, :order, :sms
-    def initialize(order = Order.new, sms: nil)
+    attr_reader :menu, :selected_dishes, :order, :menu, :sms
+    def initialize(order = Order.new, menu=Menu.new, sms: nil)
         @menu = {}
         @selected_dishes = order.ordered_dishes
         @order = order
         @sms = sms
+        @menu = menu
     end
-    def add_dish(dish)
+    def add_dish(dish)    #
         name = dish.name?
         price = dish.price?
         @menu[name] = price
