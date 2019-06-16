727f02587e8b83a8ed144ea8b981dcff3e837a35
MarcinBalejko <mbbm9@o2.pl>
2019-06-17 00:45:10 +0800

    :fire: Refactored, one test removed

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,20 +1,14 @@
 require './lib/menu.rb'
 describe Menu do
-    describe '#add_dish' do
-        it 'can add dish to the menu' do
+    describe '#select' do
+        it 'can select dishes from the menu' do
             subject.select('pizza')
             expect(subject.order[0]).to eq 'pizza'
         end
     end
-    describe '#select' do
-        it 'user can select certain dishes from the menu' do
-            subject.add_dish(dish)
-            expect(subject).to respond_to(:select).with(1).argument
-        end
         it 'selected dishes are stored' do
             subject.add_dish(dish)
             subject.select(dish)
             expect(subject.selected_dishes[0]).to eq dish
         end
     end
-end
