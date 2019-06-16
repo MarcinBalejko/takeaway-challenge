f7f002002aac18461529636e40e6313283e1a8d1
MarcinBalejko <mbbm9@o2.pl>
2019-06-17 00:48:16 +0800

    :white_check_mark: Selected dishes are stored

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -1,14 +1,13 @@
 require './lib/menu.rb'
 describe Menu do
     describe '#select' do
-        it 'can select dishes from the menu' do
+        it 'user can select certain dishes from the menu' do
+            subject.select('pizza')
+            expect(subject).to respond_to(:select).with(1).argument
+        end
+        it 'selected dishes are stored' do
             subject.select('pizza')
             expect(subject.order[0]).to eq 'pizza'
         end
     end
-        it 'selected dishes are stored' do
-            subject.add_dish(dish)
-            subject.select(dish)
-            expect(subject.selected_dishes[0]).to eq dish
-        end
-    end
+end
