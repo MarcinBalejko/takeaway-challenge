343c5294f591c365a69f0642113b3cb770d951ba
MarcinBalejko <mbbm9@o2.pl>
2019-06-17 00:51:50 +0800

    :construction: deliver method

--- spec/sms_spec.rb
+++ spec/sms_spec.rb
@@ -1,3 +1,9 @@
 require './lib/sms.rb'
 describe Sms do
+    subject(:sms) { described_class.new(client: client) }
+    let(:client) { double (:client) }
+
+    it 'delivers SMS with estimated time' do
+        sms.deliver
+    end
 end
