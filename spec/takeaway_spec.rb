e1efbfc0804e295b767318a3d4590d9dfc579742
MarcinBalejko <mbbm9@o2.pl>
2019-06-16 11:16:31 +0200

    :white_check_mark: Places the order

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -1,20 +1,28 @@
 require './lib/takeaway.rb'
 describe Takeaway do
     
+    let(:restaurant) { described_class.new }
     let(:menu) { Menu.new }
+    let(:dish) { 'pizza' }
 
     it 'has a menu' do
-        expect(subject).to respond_to(:menu)
+        expect(restaurant).to respond_to(:menu)
     end
     describe '#total' do
         it 'responds to total' do
-            expect(subject).to respond_to(:total)
+            expect(restaurant).to respond_to(:total)
         end
         it 'shows the total amount to pay' do
-            subject.menu.select('pizza')
-            subject.menu.select('lasagne')
-            expect(subject.total).to eq(30)
+            restaurant.menu.select(dish)
+            restaurant.menu.select(dish)
+            expect(restaurant.total).to eq(restaurant.menu.calculator.sum)
         end
+        it 'places the order' do
+            restaurant.menu.select(dish)
+            allow(restaurant).to receive(:require_relative)
+            restaurant.place_order
+        end
+
     end
 
 end
