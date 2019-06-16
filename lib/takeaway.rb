67167e6171eec6039fbdbb49f30f02504590c4de
MarcinBalejko <mbbm9@o2.pl>
2019-06-16 10:56:33 +0200

    :construction: Bill method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,11 +1,17 @@
 require_relative 'menu'
 class Takeaway
-    attr_reader :menu, :selected_dishes
+    attr_reader :menu, :your_order
     def initialize(menu = Menu.new)
         @menu = menu       
-        @selected_dishes = []
+        @your_order = menu.order
+
     end
     def total
         menu.calculator.sum
     end
+    def bill
+        puts menu.order
+        "Amount to pay: #{ total }"
+    end
+    
 end
