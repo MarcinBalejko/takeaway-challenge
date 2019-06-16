e31a95dad11a5c0831c4d6d1550de23f572c88e6
MarcinBalejko <mbbm9@o2.pl>
2019-06-16 10:57:35 +0200

    :construction: place_order method

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -13,5 +13,10 @@ class Takeaway
         puts menu.order
         "Amount to pay: #{ total }"
     end
+    def place_order
+        fail 'Please select your meal' if menu.order == nil
+        require_relative 'sms.rb'
+        bill
+    end
     
 end
